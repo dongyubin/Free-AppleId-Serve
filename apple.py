4e75773e424a6ff713f41f92bf631c02022a3986
itxh <892457803@qq.com>
2024-01-30 15:09:46 +0800

    更新：小火箭账号

--- apple.py
+++ apple.py
@@ -43,6 +43,9 @@ def fetch_apple_count(urls):
                     credentials.append({"account": item['username'], "password": item['password'], "country": item['country']})
             all_credentials.extend(credentials)
         elif 'get_apple_id.php' in url:
+            headers = {
+                'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
+            }
             credentials = []
             response = requests.get(url, headers=headers)
             res_text = response.text
