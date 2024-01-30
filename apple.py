ac03e0067b6e61eb994531791e0ac9e585ee4077
itxh <892457803@qq.com>
2024-01-30 14:49:27 +0800

    更新：小火箭账号

--- apple.py
+++ apple.py
@@ -27,23 +27,71 @@ def fetch_apple_count(urls):
     all_credentials = []
     # 发起请求获取网页内容
     headers = {
-        'Accept': 'application/json'
+        'Content-Type': 'application/json',
+        'Accept':'application/json, text/javascript, */*; q=0.01'
     }
     for url in urls:
-        if url.startswith('https://aunlock.laogoubi.net'):
+        # print('url:',url)
+        # if url.startswith('https://aunlock.laogoubi.net') or url.startswith('https://apple.laogoubi.net'):
+        if 'laogou' in url :
             response = requests.get(url, headers=headers)
             res_text = response.text
-            # print(res_text)
             data = json.loads(res_text)
             credentials = []
             for item in data:
-                if 'username' in item and 'password' in item:
-                    credentials.append({"account": item['username'], "password": item['password']})
+                if 'username' in item and 'password' in item and item['status']==1:
+                    credentials.append({"account": item['username'], "password": item['password'], "country": item['country']})
+            all_credentials.extend(credentials)
+        elif 'get_apple_id.php' in url:
+            credentials = []
+            response = requests.get(url, headers=headers)
+            res_text = response.text
+            data = json.loads(res_text)
+            for item in data:
+                if 'apple_id' in item and 'apple_pwd' in item:
+                    credentials.append({"account": item['apple_id'], "password": item['apple_pwd'], "country": ''})
+            all_credentials.extend(credentials)
+        elif url.startswith('https://apid.jcnf.xyz'):
+            response = requests.get(url)
+            accounts = []
+            passwords = []
+            if response.status_code == 200:
+                html_doc = response.content
+                # print(html_doc)
+                # 使用BeautifulSoup解析HTML
+                soup = BeautifulSoup(html_doc, 'html.parser')
+                button_account = soup.select_one('button.btn-primary')
+                button_password = soup.select_one('button.btn-success')
+                if button_account and button_password:
+                    accounts.append(button_account['data-clipboard-text'])
+                    passwords.append(button_password['data-clipboard-text'])
+            credentials = [{"account": a, "password": p, "country": ''} for a, p in zip(accounts, passwords)]
+            all_credentials.extend(credentials)
+        elif url.startswith('https://eg.id888.one'):
+            headers = {
+                'Content-Type':'text/html;charset=UTF-8'
+            }
+            response = requests.get(url, headers=headers, verify=False)
+            accounts = []
+            passwords = []
+            if response.status_code == 200:
+                html_doc = response.content
+                # print(html_doc)
+                # 使用BeautifulSoup解析HTML
+                soup = BeautifulSoup(html_doc, 'html.parser')
+                buttons = soup.find_all('a', {'class': 'copyBtn'})
+                for button in buttons:
+                    if '复制账号' in button.text:  
+                        accounts.append(button.get('title'))
+                    elif '复制密码' in button.text:
+                        passwords.append(button.get('title'))
+            credentials = [{"account": a, "password": p, "country": ''} for a, p in zip(accounts, passwords)]
             all_credentials.extend(credentials)
         else:
             response = requests.get(url)
             accounts = []
             passwords = []
+            account_normal_index = []
             # 检查请求是否成功
             if response.status_code == 200:
                 html_doc = response.content
@@ -53,12 +101,19 @@ def fetch_apple_count(urls):
                 # print(url, html_doc , '\n')
                 # 找到对应的button元素
                 buttons = soup.find_all('button', {'class': 'btn-outline-secondary'})
-                # print(buttons)
-                # 遍历所有匹配的元素
+                # 账号状态:获取class为card-title
+                card_status = soup.find_all(class_='card-title')
+                print(card_status)
+
+                for i,card_statu in enumerate(card_status):
+                    if '正常' in card_statu.get_text():
+                        account_normal_index.append(i)
+                        # print(i)
+                        # 遍历所有匹配的元素
                 for button in buttons:
                     # 获取onclick属性的值
                     onclick_value = button.get('onclick')
-                    if '复制帐号' in button.text and '账号维护中' not in onclick_value:
+                    if '复制帐号' in button.text and '维护中' not in onclick_value:
                         start_index = onclick_value.find("(") + 1
                         end_index = onclick_value.find(")")
                         account = onclick_value[start_index:end_index].replace("'", "")
@@ -68,18 +123,20 @@ def fetch_apple_count(urls):
                         end_index = onclick_value.find(")")
                         password = onclick_value[start_index:end_index].replace("'", "")
                         passwords.append(password)
-            # 结合账号和密码
+                # 结合账号和密码
                 if len(passwords) == 1:
                     passwords = passwords * len(accounts)
-                credentials = [{"account": a, "password": p} for a, p in zip(accounts, passwords)]
-                all_credentials.extend(credentials)
+                for j in account_normal_index:
+                    credentials = [{"account": a, "password": p, "country": ''} for a, p in zip([accounts[j]], [passwords[j]])]
+                    all_credentials.extend(credentials)
             else:
                 print(f"Failed to retrieve the webpage from {url}")
-    # print(all_credentials)
+    print(all_credentials)
     return [
         {
             'account': account['account'],
-            'password': account['password']
+            'password': account['password'],
+            'country': account['country']
         }
         for account in all_credentials
         ]
@@ -95,8 +152,8 @@ if __name__ == "__main__":
 
     # 构建账号信息的Markdown格式
     entries_md = "\n".join([
-        "* 账号：`{account}` \n * 密码：`{password}`".format(**account)
-        for account in accounts
+        "--------- {i} ---------\n* {country}账号：`{account}`\n* 密码：`{password}`".format(i=i+1, **account)
+        for i, account in enumerate(accounts)
     ])
 
     # 更新账号信息
